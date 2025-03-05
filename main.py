d11c764fa2b2cbd1430699e332bef0c396bffb40
RaphaelSapia <raphael-sapia@hotmail.com>
2025-03-04 23:39:02 -0300

    Inicial

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from flask import Flask, jsonify
 import requests
 from datetime import datetime
 
-requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+app = Flask(__name__)
 
-requisicao_dic = requisicao.json()
-cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
-cotacao_euro = requisicao_dic["EURBRL"]["bid"]
-cotacao_btc = requisicao_dic["BTCBRL"]["bid"]
+@app.route("/cotacoes")
+def cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+    requisicao_dic = requisicao.json()
+    cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
+    cotacao_euro = requisicao_dic["EURBRL"]["bid"]
+    cotacao_btc = requisicao_dic["BTCBRL"]["bid"]
+    dados = {
+        "atualizado_em": datetime.now().isoformat(),
+        "dolar": cotacao_dolar,
+        "euro": cotacao_euro,
+        "btc": cotacao_btc
+    }
+    return jsonify(dados)
 
-print(f"Cotação Atualizada. {datetime.now()}\nDólar: R${cotacao_dolar}\nEuro: R${cotacao_euro}\nBTC: R${cotacao_btc}")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
